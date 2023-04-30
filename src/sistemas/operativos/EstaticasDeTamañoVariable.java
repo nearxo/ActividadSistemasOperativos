63df5f9e2aafb32a61e9167ef74c3e99cd182dfd
nearxo <66927680+nearxo@users.noreply.github.com>
2023-04-29 23:53:39 -0500

    INICIO
    inicio

---
+++ src/sistemas/operativos/EstaticasDeTamañoVariable.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemas.operativos;
+
+/**
+ *
+ * @author Thomas
+ */
+public class EstaticasDeTamañoVariable extends Estrategias {
+
+    public EstaticasDeTamañoVariable(String pa) {
+    }
+
+    @Override
+    public int posicionProceso(String Estrategia) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
